469219f1b3d96430103550474d3973895061a454
anton9dpro <anton@9d.pro>
2026-05-15 17:50:19 +0300

    changes

--- src/data/giftProducts.ts
+++ src/data/giftProducts.ts
@@ -6,16 +6,14 @@ export interface GiftProduct {
 
 export const giftProducts: GiftProduct[] = [
   { id: 'gift-01', name: 'Набор магнитов', price: 3 },
-  { id: 'gift-02', name: 'Кружка + сертификат', price: 13 },
+  { id: 'gift-02', name: 'Кружка', price: 8 },
   { id: 'gift-03', name: 'Сертификат', price: 5 },
-  { id: 'gift-04', name: 'Коврик + сертификат', price: 12 },
+  { id: 'gift-04', name: 'Коврик', price: 7 },
   { id: 'gift-05', name: 'Носки', price: 6 },
   { id: 'gift-06', name: 'Носки + сертификат', price: 11 },
-  { id: 'gift-07', name: 'Коврик', price: 7 },
-  { id: 'gift-08', name: 'Кружка + магнит', price: 9 },
-  { id: 'gift-09', name: 'Кружка', price: 8 },
-  { id: 'gift-10', name: 'Шоппер', price: 12 },
-  { id: 'gift-11', name: 'Сертификат + магнит', price: 6 },
-  { id: 'gift-12', name: 'Коврик + магнит', price: 8 },
-  { id: 'gift-13', name: 'Носки + магнит', price: 7 },
+  { id: 'gift-07', name: 'Шампанское', price: 12 },
+  { id: 'gift-8', name: 'Шоппер', price: 12 },
+  { id: 'gift-9', name: 'Сертификат + магнит', price: 6 },
+  { id: 'gift-10', name: 'Носки + магнит', price: 7 },
+  { id: 'gift-11', name: 'Шампанское + магнит', price: 13 },
 ];
